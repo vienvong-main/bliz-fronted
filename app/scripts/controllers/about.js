4daa9d7f319f6bb18cbe53c90aa3af220d2598e9
vietpn <vietpnk53@gmail.com>
2015-12-11 11:31:35 +0700

    update

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blizFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blizFrontendApp
+ */
+angular.module('blizFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blizFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blizFrontendApp
+ */
+angular.module('blizFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
